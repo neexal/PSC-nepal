aafe898cd7e10b0e30090b38e78d1dcc84a5ef9d
Nischal Ghimire <nishanghimire111@gmail.com>
2025-11-21 16:30:47 +0100

    err

--- backend/psc_nepal/urls.py
+++ backend/psc_nepal/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from quizzes.views import (
     QuizViewSet, QuestionViewSet, ResultViewSet, register, login,
     StudyMaterialViewSet, NotificationViewSet, UserProfileViewSet, analytics,
     SubjectViewSet, BadgeViewSet, StreakViewSet, BookmarkViewSet, QuestionReportViewSet,
